539a5890b5a31fe781760197a9e8474c48156340
VíctorBlasco5 <victor.blasco.17@gmail.com>
2024-04-28 12:18:39 +0200

    feat: registerUser function in apiCalls and logic implemented in register

--- src/common/CInput/CInput.jsx
+++ src/common/CInput/CInput.jsx
@@ -1,13 +1,16 @@
 import "./CInput.css"
 
-export const CInput = ({ type, name, value, changeEmit}) => {
-    return (
+export const CInput = ({ placeholder, type, name, nickname, value, disabled, changeEmit, onBlurFunction}) => {    return (
         <input 
         className="cInputDesign"
+        placeholder={placeholder}
         type={type}
         name={name}
+        nickname={nickname}
         value={value}
+        disabled={disabled}
         onChange={(e)=>changeEmit(e)}
+        onBlur={onBlurFunction}
         />
     )
 }
