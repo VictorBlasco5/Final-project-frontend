87b1cffb8d1972f38668a3158078e9462a44acd3
VíctorBlasco5 <victor.blasco.17@gmail.com>
2024-05-05 15:26:34 +0200

    feat: home view design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,39 @@
 import "./Home.css";
+import ball from "../../../img/logo.png";
+import amigo from "../../../img/friends.png";
+import number1 from "../../../img/n1.png";
+import court from "../../../img/court.png";
+
 
 export const Home = () => {
     return (
-        <div className="homeDesign">
-            <h1>Home</h1>
+        <div className="homeDesign"
+        >
+            <div className="leftHome">
+                <img className="logoHome" src={ball} alt="ball" />
+            </div>
+            <div>
+                <div className="textHome">
+                    <div>NOMBRE</div>
+                    <div>La web número 1 de España para jugar y hacer amigos</div>
+
+                </div>
+                <div className="downHome">
+                    <div className="friends">
+                        <img className="logoFriends" src={amigo} alt="Amigos" />
+                        <div>Haz amigos tio</div>
+                    </div>
+                    <div className="numberOne">
+                        <img className="logoNumberOne" src={number1} alt="Número 1" />
+                        <div>Semos los megore</div>
+                    </div>
+                    <div className="court">
+                        <img className="logoCourt" src={court} alt="Pista" />
+                        <div>Moltos matches</div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
